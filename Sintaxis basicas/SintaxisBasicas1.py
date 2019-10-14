44dca9aa3940e502f8d5fc7198f67a61c4df9130
Leonardo R <lfavio2@gmail.com>
2019-10-14 12:50:46 -0500

    se agrega la sintaxis básica 1

---
+++ Sintaxis basicas/SintaxisBasicas1.py
@@ -0,0 +1,22 @@
+#Así se comenta con el caracter #
+#Esta línea está comentada
+
+#Imprimir en pantalla con el método print()
+print("Hola mundo")
+
+#Declarar variable
+mi_nombre ="mi nombre es python"
+print(mi_nombre)
+
+#Texto multilínea con el caracter \
+texto = "este es un texto\
+ multilínea"
+print(texto)
+
+#Identación
+a=0
+for i in range(5):
+    a+=1
+    print(a)
+
+
